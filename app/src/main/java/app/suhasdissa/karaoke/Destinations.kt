bfcc4e71720fc22966c95bb4c90ba5892c44d717
Suhas Dissanayake <suhasdissa@protonmail.com>
2023-06-23 20:05:59 +0530

    Cleanup Destinations.kt

--- app/src/main/java/app/suhasdissa/karaoke/Destinations.kt
+++ app/src/main/java/app/suhasdissa/karaoke/Destinations.kt
@@ -12,10 +12,6 @@ object Home : Destination {
     override val route = "home"
 }
 
-object PlayerScreen : Destination {
-    override val route = "playerscreen"
-}
-
 object AudioRecorderScreen : Destination {
     override val route = "recorderscreen"
 }
@@ -33,39 +29,3 @@ object YtPlayerRecorderScreen : Destination {
     val routeWithArgs = "$route/{id}"
     val arguments = listOf(navArgument("id") { type = NavType.StringType })
 }
-/*
-object FunnyVideoView : Destination {
-    override val route = "funvidview"
-}
-
-object FeedView : Destination {
-    override val route = "feedview"
-}
-
-object Settings : Destination {
-    override val route = "settings"
-}
-
-object About : Destination {
-    override val route = "about"
-}
-
-object PhotoView : Destination {
-    override val route = "memescreen"
-    val routeWithArgs = "$route/{url}"
-    val arguments = listOf(navArgument("url") { type = NavType.StringType })
-}
-
-object VideoPlayer : Destination {
-    override val route = "video"
-    val routeWithArgs = "$route/{url}"
-    val arguments = listOf(navArgument("url") { type = NavType.StringType })
-}
-
-object TextViewer : Destination {
-    override val route = "textviewer"
-    val routeWithArgs = "$route/{text}"
-    val arguments = listOf(navArgument("text") { type = NavType.StringType })
-}
-
-*/
