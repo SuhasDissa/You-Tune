cdfb264361ac96546148196a16fcedb8fc437e85
Suhas Dissanayake <suhasdissa@protonmail.com>
2023-07-31 14:57:55 +0530

    Improve audio recorder UI

--- app/src/main/java/app/suhasdissa/karaoke/ui/components/AudioRecordController.kt
+++ app/src/main/java/app/suhasdissa/karaoke/ui/components/AudioRecordController.kt
@@ -2,27 +2,37 @@ package app.suhasdissa.karaoke.ui.components
 
 import android.os.Build
 import android.text.format.DateUtils
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Mic
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Stop
-import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import app.suhasdissa.karaoke.R
@@ -36,56 +46,103 @@ fun AudioRecordController(recorderViewModel: RecorderViewModel = viewModel()) {
         Modifier
             .fillMaxWidth()
             .padding(bottom = 40.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
-        Text(
-            text = DateUtils.formatElapsedTime(recorderViewModel.recordedTime),
-            style = MaterialTheme.typography.titleLarge
-        )
-        Spacer(modifier = Modifier.height(20.dp))
+        recorderViewModel.recordedTime.let {
+            Text(
+                text = DateUtils.formatElapsedTime(it),
+                style = MaterialTheme.typography.displayLarge
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+        }
         Row(
             verticalAlignment = Alignment.CenterVertically
         ) {
-            FloatingActionButton(onClick = {
-                when {
-                    recorderViewModel.recorderState != RecorderState.IDLE -> recorderViewModel.stopRecording()
-                    else -> recorderViewModel.startAudioRecorder(context)
-                }
-            }) {
-                Icon(
-                    imageVector = when {
-                        recorderViewModel.recorderState != RecorderState.IDLE -> Icons.Default.Stop
-                        else -> Icons.Default.Mic
-                    }, contentDescription = stringResource(
-                        if (recorderViewModel.recorderState != RecorderState.IDLE) {
-                            R.string.stop
-                        } else {
-                            R.string.record
-                        }
-                    )
+            ElevatedCard(
+                colors = CardDefaults.elevatedCardColors(
+                    containerColor = if (isSystemInDarkTheme()) {
+                        Color(0xA8EE665B)
+                    } else {
+                        Color(
+                            0xffdd6f62
+                        )
+                    },
+                    contentColor = Color.White
+                ),
+                shape = CircleShape
+            ) {
+                val buttonDescription = stringResource(
+                    if (recorderViewModel.recorderState != RecorderState.IDLE) {
+                        R.string.stop
+                    } else {
+                        R.string.record
+                    }
                 )
+                IconButton(
+                    onClick = {
+                        when {
+                            recorderViewModel.recorderState != RecorderState.IDLE -> recorderViewModel.stopRecording()
+                            else -> recorderViewModel.startAudioRecorder(context)
+                        }
+                    },
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .semantics { contentDescription = buttonDescription }
+                ) {
+                    when {
+                        recorderViewModel.recorderState != RecorderState.IDLE -> {
+                            Icon(
+                                Icons.Default.Stop,
+                                modifier = Modifier.size(36.dp),
+                                contentDescription = stringResource(R.string.pause)
+                            )
+                        }
+
+                        else -> {
+                            Box(
+                                Modifier
+                                    .size(40.dp)
+                                    .clip(CircleShape)
+                                    .background(Color(0x9FFFFFFF))
+                            )
+                            Box(
+                                Modifier
+                                    .size(26.dp)
+                                    .clip(CircleShape)
+                                    .background(Color(0x9FFFFFFF))
+                            )
+                        }
+                    }
+                }
             }
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N && recorderViewModel.recorderState != RecorderState.IDLE) {
                 Spacer(modifier = Modifier.width(20.dp))
-                ClickableIcon(
-                    imageVector = if (recorderViewModel.recorderState == RecorderState.PAUSED) {
-                        Icons.Default.PlayArrow
-                    } else {
-                        Icons.Default.Pause
-                    }, contentDescription = stringResource(
-                        if (recorderViewModel.recorderState == RecorderState.PAUSED) {
-                            R.string.resume
-                        } else {
-                            R.string.pause
-                        }
+                ElevatedCard(
+                    colors = CardDefaults.elevatedCardColors(
+                        containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                        contentColor = MaterialTheme.colorScheme.onSecondaryContainer
                     )
                 ) {
-                    if (recorderViewModel.recorderState == RecorderState.PAUSED) {
-                        recorderViewModel.resumeRecording()
-                    } else {
-                        recorderViewModel.pauseRecording()
+                    ClickableIcon(
+                        imageVector = if (recorderViewModel.recorderState == RecorderState.PAUSED) {
+                            Icons.Default.PlayArrow
+                        } else {
+                            Icons.Default.Pause
+                        },
+                        contentDescription = stringResource(
+                            if (recorderViewModel.recorderState == RecorderState.PAUSED) {
+                                R.string.resume
+                            } else {
+                                R.string.pause
+                            }
+                        )
+                    ) {
+                        if (recorderViewModel.recorderState == RecorderState.PAUSED) {
+                            recorderViewModel.resumeRecording()
+                        } else {
+                            recorderViewModel.pauseRecording()
+                        }
                     }
                 }
             }
--- app/src/main/java/app/suhasdissa/karaoke/ui/components/AudioVisualizer.kt
+++ app/src/main/java/app/suhasdissa/karaoke/ui/components/AudioVisualizer.kt
@@ -4,32 +4,41 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.drawscope.translate
 import androidx.lifecycle.viewmodel.compose.viewModel
 import app.suhasdissa.karaoke.backend.viewmodels.RecorderViewModel
-
 @Composable
 fun AudioVisualizer(
     modifier: Modifier = Modifier
 ) {
     val viewModel: RecorderViewModel = viewModel()
-    val maxAmplitude = 3000
+    /*
+    * set max amplitude to tune the sensitivity
+    * Higher the value, lower the sensitivity of the visualizer
+     */
+    val maxAmplitude = 10000
     val primary = MaterialTheme.colorScheme.primary
+    val primaryMuted = primary.copy(alpha = 0.3f)
+    val amplitudes = viewModel.recordedAmplitudes
     Canvas(modifier = modifier) {
-        val height = this.size.height / 2
-        val width = this.size.width * 0.75f
-        viewModel.recordedAmplitudes.forEachIndexed { index, amplitude ->
-            translate(width, height) {
-                val boxHeight = (height * (amplitude.toFloat() / maxAmplitude))
+        val height = this.size.height / 2f
+        val width = this.size.width
+
+        translate(width, height) {
+            amplitudes.forEachIndexed { index, amplitude ->
+                val amplitudePercentage = (amplitude.toFloat() / maxAmplitude).coerceAtMost(1f)
+                val boxHeight = height * amplitudePercentage
                 drawRoundRect(
-                    color = primary,
+                    color = if (amplitudePercentage > 0.05f) primary else primaryMuted,
                     topLeft = Offset(
-                        15f * (index - viewModel.recordedAmplitudes.size),
-                        -boxHeight / 2
+                        30f * (index - viewModel.recordedAmplitudes.size),
+                        -boxHeight / 2f
                     ),
-                    size = Size(10f, boxHeight)
+                    size = Size(15f, boxHeight),
+                    cornerRadius = CornerRadius(3f, 3f)
                 )
             }
         }
