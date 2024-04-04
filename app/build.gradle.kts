b4ea1c15b8a93f499351574886766dff766a5872
Suhas Dissanayake <suhasdissa@gmail.com>
2024-04-04 19:45:01 +0530

    chore: bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "app.suhasdissa.karaoke"
         minSdk = 21
         targetSdk = 33
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
